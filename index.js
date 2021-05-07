e186aa06f8780fef1d0200c4659b87bdafe7562c
jeffreyfevre <58734152+jeffreyfevre@users.noreply.github.com>
2021-05-07 09:10:52 +0200

    v1 chron

--- index.js
+++ index.js
@@ -9,11 +9,39 @@ const config = require("./config");
 const client = new Discord.Client();
 const axios = require("axios");
 const cheerio = require("cheerio");
+var cron = require('node-cron');
+client.login(config.BOT_TOKEN).then(() => {
+  console.log("Bot connecté !");
+});
+console.log(JSON.stringify(client));
 
 let edtApiEndpoint =
   "https://edtmobiliteng.wigorservices.net//WebPsDyn.aspx?action=posEDTBEECOME&serverid=C&Tel=a.biranielhbak&date=05/06/2021&hashURL=e8cc7097357cd48891d5fca9d00db8ace0ecc236cd0a6e236d3268eee1efbcb3e7bbe1645dca9f2e9329a1e9798d90ffb7217ddc9c4c90ef75b36cc0a903f02b";
 /* let edtApiEndpoint =
   "https://edtmobiliteng.wigorservices.net//WebPsDyn.aspx?action=posEDTBEECOME&serverid=C&Tel=jeffrey.fevre&date=05/06/2021&hashURL=f12bc7a9ff71af4dd7294ff941d3c5cd8eaca414ca422c12acf3d7cfde77a6544a49a0491e5365e78828502dfaf33aa511cc201821fc21921870efb32c54c50d"; */
+
+let edtSave;
+
+cron.schedule('* * * * *', async () => {
+  console.log('running every minute 1, 2, 4 and 5');
+
+
+  let date = new Date();
+  let heure = date.getHours();
+  let min = date.getMinutes();
+  let edtResponse;
+  if (heure == "8"&& min == "16" || heure == "13" && min == "16")
+  {
+    edtResponse = await fetchLink();
+  }
+
+  if (edtResponse != edtSave)
+  {
+    const channel = client.channels.cache.get("828897751844257795");
+      channel.send(edtResponse);
+  }
+
+});
 
 client.on("message", async function (message) {
   const prefix = "!";
@@ -39,10 +67,12 @@ client.on("message", async function (message) {
   }
 });
 
-client.login(config.BOT_TOKEN).then(() => {
-  console.log("Bot connecté !");
-});
 
+client.on('ready', () => {
+  console.log(client.channels);
+  client.channels.fetch("828897751844257795")
+});
+console.log(JSON.stringify(client.channels))
 /**
  *
  * @param {number} retryCount
@@ -76,12 +106,9 @@ async function getEDTLink() {
   const edtApiResponse = await axios.get(edtApiEndpoint);
   const edtBody = edtApiResponse.data;
   const $ = cheerio.load(edtBody);
-
   const allLinks = $("div.Teams a:first-child");
-
+  console.log(allLinks);
   const link = allLinks.last().attr("href");
-
   console.log(`link found : `, link);
-
   return { link, count: allLinks.length };
 }
